4e8479ca2c558de10b3033ddb099d07571502fa8
svanlig <149100660+svanlig@users.noreply.github.com>
2026-09-04 08:50:50 -0400

    Update fetch URL in app.js
    http://127.0.0.1:5000/import-url line 372

--- app.js
+++ app.js
@@ -369,7 +369,7 @@ async function importWebsite() {
 
         const response =
             await fetch(
-                "http://127.0.0.1:5000/import-url",
+                "/import-url",
                 {
                     method: "POST",
 
